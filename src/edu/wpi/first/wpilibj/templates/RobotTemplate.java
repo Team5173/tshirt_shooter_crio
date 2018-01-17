7739d63be968e395189c25516fa52ff9ad08d4b0
Driver <Driver@10.147.1.18>
2018-01-17 05:55:30 +0400

    Java code with the autonomous  new code for solenoids and other

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class RobotTemplate extends SimpleRobot{
 
@@ -16,41 +17,52 @@ public RobotTemplate()
 }
 
 RobotDrive chassis = new RobotDrive(1, 2);
-Joystick leftStick = new Joystick(1);
+Joystick Controller = new Joystick(1);
 Relay myRelay = new Relay(2);
+Solenoid solenoid = new Solenoid(1);
 
 public void autonomous() {
     chassis.setSafetyEnabled (false);
     //drive forward halfspeed
     chassis.drive(-0.5, 0.0);
-    //drives 1 seconds
-    Timer.delay(1.0);
+    //drive 1 second
+    Timer.delay(5.0);
     //spin right
-    chassis.drive(0, 0.5);
-    //spin 5 seconds
-    Timer.delay(2.0);
+    chassis.drive(0.0, 0.5);
+    //spin 2 seconds
+    Timer.delay(5.0);
     //spin left
     chassis.drive(0.0, -0.5);
-    //spin for 5 seconds
-    Timer.delay(2.0);
+    //spin for 2 seconds
+    Timer.delay(5.0);
     //Drive in reverse
-    chassis.drive(0.5,0.0);
+    chassis.drive(0.5, 0.0);
+    //drive 1 second
+    Timer.delay(5.0);
     //stop
-    Timer.delay(1.0);
-    
-}
+    chassis.drive(0.0, 0.0);
+    }
 
 public void operatorControl() {
     chassis.setSafetyEnabled(true);
+    
     while (isOperatorControl() && isEnabled()){
-        chassis.arcadeDrive(leftStick);
-        if (leftStick.getRawAxis(3) > .1) {
+        chassis.arcadeDrive(Controller);
+        if (Controller.getRawAxis(3) > .1) {
             myRelay.set(Relay.Value.kForward);
         }
       else {
            myRelay.set(Relay.Value.kOff);
        }
+        if(Controller.getRawAxis(1) > .1) {
+           solenoid.set(true);
+       }
+      else {
+           solenoid.set(false);
+        }
         Timer.delay(0.01);
     }
+    
 }
+
 }
